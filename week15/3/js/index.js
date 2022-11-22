a09a4173881b4e52930f91e46726b6abbebaf7b5
LimpLizket <esaulenko.elizaveta@mail.ru>
2022-11-22 05:11:58 +0300

    theme change done

--- week15/3/js/index.js
+++ week15/3/js/index.js
@@ -1,39 +1,7 @@
-let op; 
-function func() {
-  let result;
-  let num1 = Number(document.getElementById("num1").value);
-  let num2 = Number(document.getElementById("num2").value);
-  if(num1 > 0 && num2 > 0){
-    switch (op) {
-    case '+':
-      result = num1 + num2;
-      break;
-    case '-':
-      result = num1 - num2;
-      break;
-    case '*':
-      result = num1 * num2;
-      break;
-    case '/':
-      result = num1 / num2;
-      break;
-  }
-}
-  if(num1 === 0 || num2 === 0) {
-  switch(op) {
-    case '+':
-      result = num1 + num2;
-      break;
-    case '-':
-      result = num1 - num2;
-      break;
-    case '*':
-      result = num1 * num2;
-      break;
-    case '/':
-      result = alert('На ноль делить нельзя!');
-      break;
-  }
-}
-  document.getElementById("result").innerHTML = result;
-}
+let page = document.querySelector('.page');
+let themeButton = document.querySelector('.theme-button');
+
+themeButton.onclick = function() {
+  page.classList.toggle('light-theme');
+  page.classList.toggle('dark-theme');
+};
